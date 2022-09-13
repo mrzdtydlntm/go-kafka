4edc31a9f17dd4e18d1d99b0d9ac9981fa2ba2a8
mrzdtydlntm <mrzdtydlntm@gmail.com>
2022-09-13 15:23:00 +0700

    fix: bug subscriber

--- controllers/subscriber.go
+++ controllers/subscriber.go
@@ -2,18 +2,29 @@ package controllers
 
 import (
 	"context"
+	"log"
 	"watermillTutorial/handlers"
 
 	"github.com/ThreeDotsLabs/watermill/message"
 )
 
 // SubscribeFromKafka is a controller function to subscribe all messages from kafka
-func SubscribeFromKafka(host, consumerGroup string) <-chan *message.Message {
+func SubscribeFromKafka(host, consumerGroup, topic string) {
 	subscriber := handlers.SubscriberHandler(host, consumerGroup)
-	messages, err := subscriber.Subscribe(context.Background(), "allobank")
+	messages, err := subscriber.Subscribe(context.Background(), topic)
 	if err != nil {
 		panic(err)
 	}
 
-	return messages
+	go process(messages)
+}
+
+func process(messages <-chan *message.Message) {
+	for msg := range messages {
+		log.Printf("received message: %s, payload: %s", msg.UUID, string(msg.Payload))
+
+		// we need to Acknowledge that we received and processed the message,
+		// otherwise, it will be resent over and over again.
+		msg.Ack()
+	}
 }
